96a26f88d132f9408536822c46a95e95074e2e51
kishore-devaraj <kishoregrylls@gmail.com>
2018-07-18 11:31:51 +0530

    Basic skeleton required to build a reusable component for react

---
+++ src/Component.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './component.css'
+
+const Component = () => {
+  return (
+    <div>Write your component in Component.js</div>
+  )
+}
+
+export default Component
